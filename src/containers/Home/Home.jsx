b8ddac43390572ea2fc6f3f769812e149f698eb1
Alexlg <Alejandrolg86@gmail.com>
2022-08-18 11:58:45 +0200

    feat: Home container basic structure created

---
+++ src/containers/Home/Home.jsx
@@ -0,0 +1,16 @@
+import React from "react";
+
+import "./Home.css"
+
+const Home = props => {
+    return (
+        <div className="mainBox">
+            <div className="headerBox">HEADER</div>
+            <div className="menuBox">MENU</div>
+            <div className="contentBox">CONTENT</div>
+            <div className="footerBox">FOOTER</div>
+        </div>
+    )
+}
+
+export default Home;
